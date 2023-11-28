6d6871ac357593fa9f61311d8883db628e6291b6
Dario Cruz Minoza Jr <dminzodev@gmail.com>
2023-11-28 23:40:42 +0800

    added developer settings under profile

--- project/spa/src/pages/Authentication/Profile/partials/developerSettings.js
+++ project/spa/src/pages/Authentication/Profile/partials/developerSettings.js
@@ -1,20 +1,17 @@
 import { useSelector } from "react-redux"
 
-export default function CompanySettings() {
-    const company = useSelector((state) => state.authentication.company)
+export default function DeveloperSettings () {
+    const auth = useSelector((state) => state.authentication)
     return (
         <>
             <div className="p-4 pb-0 mb-0 bg-white border-b-0 rounded-t-2xl">
-                <h6>Company Settings</h6>
+                <h6>Developer Settings</h6>
             </div>
             <div className="flex-auto p-4">
                 <div className="font-bold leading-tight uppercase text-xs text-slate-500">
                     <ul className="flex flex-col pl-0 mb-0 rounded-lg">
                         <li className="relative block px-0 py-2 bg-white border-0 rounded-t-lg text-inherit">
-                            <b>Name: </b> {company.name}
-                        </li>
-                        <li className="relative block px-0 py-2 bg-white border-0 rounded-t-lg text-inherit">
-                            <b>Website: </b> {company.website}
+                            <b>Token: </b> {auth.token}
                         </li>
                     </ul>
                 </div>
